9582705b4b4f8582bc44e1f2909051a7a7dc7d05
Choseongyul <tjdduf0504@naver.com>
2024-05-28 01:39:54 +0900

    웹프로그래밍 과제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // 상태를 저장할 useState 훅 사용
+  const [data, setData] = useState(null);
+
+  // 데이터 받아오기
+  const fetchData = async () => {
+    try {
+      // fetch로 데이터 요청
+      const response = await fetch('http://incheon/web');
+      const jsonData = await response.json();
+
+      // 로컬 스토리지에 데이터 저장
+      localStorage.setItem('web', JSON.stringify(jsonData));
+
+      // 상태 업데이트
+      setData(jsonData);
+    }
+    catch (error) {
+      console.error('Error fetching data:', error); //fetch 실패하면 콘솔에 에러 메세지
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchData}>INU</button>
+      <div>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'fetch 실패'}
+      </div>
     </div>
   );
 }
