40d2b2f5878933051b42de5dff68981e53eeb59f
Choseongyul <tjdduf0504@naver.com>
2024-05-28 03:28:55 +0900

    webprograming quiz code 최종

--- src/App.js
+++ src/App.js
@@ -3,32 +3,29 @@ import React, { useState } from "react";
 function App() {
   const [data, setData] = useState([]);
 
-  async function fetchData() {
-    //예외처리
+  async function fetchWeb() { //비동기 함수선언
     try {
-      const response = await fetch("http://localhost:3002/web"); //fetch
-      const result = await response.json();
+      const response = await fetch("http://localhost:3002/web"); // db.json에서 원하는 값 fetch하기
+      const result = await response.json(); // result변수에 json으로 변환한 값을 저장
 
-      localStorage.setItem("web", JSON.stringify(result));
-      const storedData = JSON.parse(localStorage.getItem("web"));
+      localStorage.setItem("web", JSON.stringify(result)); //로컬스토리지에 저장
+      const storedData = JSON.parse(localStorage.getItem("web")); //저장된 값 불러오기
 
       setData(Array.isArray(storedData) ? storedData : []);
     }
-      
     catch(err) {
-      console.error("fetch실패", err);
+      console.error("fetch실패", err); //fetch 실패 예외처리
     }
   }
 
   return (
     <div className="App">
-      <button onClick={fetchData}>INU</button>
+      <button onClick={fetchWeb}>INU</button> {/* 버튼 */}
     
       <div>
-        {data.map((item, index) => (<p key={index}>{item.class}</p>))}
+        {data.map((item, index) => (<p key={index}>{item.class}</p>))} {/* p태그로 출력 */}
       </div>
     </div>
   );
 }
-
 export default App;
